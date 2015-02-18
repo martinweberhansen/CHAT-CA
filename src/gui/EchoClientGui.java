ecffeac2584a772f2177c59c0359376bd9a061c4
Janus <Janus@10.50.131.84>
2015-02-18 15:48:57 +0100

    GUI changes...

--- src/gui/EchoClientGui.java
+++ src/gui/EchoClientGui.java
@@ -11,14 +11,18 @@ import echoclient.EchoListener;
 /**
  * @author MASA
  */
-public class EchoClientGui extends javax.swing.JFrame  implements EchoListener{
-EchoClient client = new EchoClient();
+public class EchoClientGui extends javax.swing.JFrame implements EchoListener
+{
+    EchoClient client;
+    
+    private boolean online = false;
+    
     /**
      * Creates new form EchoClientGui
      */
-    public EchoClientGui() {
+    public EchoClientGui()
+    {
         initComponents();
-        msgInputField.setText(null); // ? ? ?? ? ? ? ? ?? ?  ? ?? ? ? ?? ? ? ?? ?? ? ? ? ? ? ?? ? ? ? ? ? ?? ? ? ?? ? ? ? ?
     }
 
     /**
@@ -30,71 +34,239 @@ EchoClient client = new EchoClient();
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        msgOutputField = new javax.swing.JTextArea();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        msgInputField = new javax.swing.JTextArea();
+        jDialogNewConnection = new javax.swing.JDialog();
+        jPanel2 = new javax.swing.JPanel();
+        newConnectionServerAddress = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        newConnectionPort = new javax.swing.JTextField();
+        newConnectionConnect = new javax.swing.JButton();
+        newConnectionCancel = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        newConnectionUserName = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        jButtonLoginLogout = new javax.swing.JButton();
+        jTextFieldMessage = new javax.swing.JTextField();
+        jButtonMessageSend = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextAreaChat = new javax.swing.JTextArea();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jDialogNewConnection.setTitle("New Connection");
+        jDialogNewConnection.setResizable(false);
 
-        jButton1.setText("send");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jPanel2.setPreferredSize(new java.awt.Dimension(400, 300));
+
+        newConnectionServerAddress.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        newConnectionServerAddress.setText("localhost");
+        newConnectionServerAddress.setPreferredSize(new java.awt.Dimension(220, 30));
+
+        jLabel1.setText("Serveraddress:");
+        jLabel1.setPreferredSize(new java.awt.Dimension(120, 30));
+
+        jLabel2.setText("Port:");
+        jLabel2.setPreferredSize(new java.awt.Dimension(80, 30));
+
+        newConnectionPort.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        newConnectionPort.setText("9090");
+        newConnectionPort.setPreferredSize(new java.awt.Dimension(100, 30));
+
+        newConnectionConnect.setText("Connect");
+        newConnectionConnect.setPreferredSize(new java.awt.Dimension(120, 30));
+        newConnectionConnect.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                newConnectionConnectActionPerformed(evt);
             }
         });
 
-        msgOutputField.setColumns(20);
-        msgOutputField.setRows(5);
-        jScrollPane1.setViewportView(msgOutputField);
+        newConnectionCancel.setText("Cancel");
+        newConnectionCancel.setPreferredSize(new java.awt.Dimension(120, 30));
+        newConnectionCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                newConnectionCancelActionPerformed(evt);
+            }
+        });
 
-        msgInputField.setColumns(20);
-        msgInputField.setRows(5);
-        jScrollPane2.setViewportView(msgInputField);
+        jLabel3.setText("Username:");
+        jLabel3.setPreferredSize(new java.awt.Dimension(120, 30));
 
-        jLabel1.setText("Recieved msg");
+        newConnectionUserName.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        newConnectionUserName.setPreferredSize(new java.awt.Dimension(220, 30));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(newConnectionCancel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20)
+                        .addComponent(newConnectionConnect, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(20, 20, 20)
+                                .addComponent(newConnectionUserName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(jPanel2Layout.createSequentialGroup()
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGap(20, 20, 20)
+                                    .addComponent(newConnectionServerAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(jPanel2Layout.createSequentialGroup()
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGap(20, 20, 20)
+                                    .addComponent(newConnectionPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(newConnectionUserName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(50, 50, 50)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(newConnectionServerAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(newConnectionPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(newConnectionConnect, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(newConnectionCancel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30))
+        );
+
+        javax.swing.GroupLayout jDialogNewConnectionLayout = new javax.swing.GroupLayout(jDialogNewConnection.getContentPane());
+        jDialogNewConnection.getContentPane().setLayout(jDialogNewConnectionLayout);
+        jDialogNewConnectionLayout.setHorizontalGroup(
+            jDialogNewConnectionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        jDialogNewConnectionLayout.setVerticalGroup(
+            jDialogNewConnectionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(605, 625));
+        setPreferredSize(new java.awt.Dimension(600, 600));
+        setResizable(false);
+
+        jPanel1.setPreferredSize(new java.awt.Dimension(600, 600));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButtonLoginLogout.setText("Login");
+        jButtonLoginLogout.setPreferredSize(new java.awt.Dimension(120, 30));
+        jButtonLoginLogout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonLoginLogoutActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButtonLoginLogout, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
+
+        jTextFieldMessage.setPreferredSize(new java.awt.Dimension(420, 30));
+        jTextFieldMessage.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldMessageActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextFieldMessage, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 550, -1, -1));
+
+        jButtonMessageSend.setText("Send");
+        jButtonMessageSend.setPreferredSize(new java.awt.Dimension(120, 30));
+        jButtonMessageSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonMessageSendActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButtonMessageSend, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 550, -1, -1));
+
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setToolTipText("Chat");
+        jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(560, 460));
+
+        jTextAreaChat.setEditable(false);
+        jTextAreaChat.setColumns(20);
+        jTextAreaChat.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
+        jTextAreaChat.setRows(5);
+        jTextAreaChat.setWrapStyleWord(true);
+        jScrollPane1.setViewportView(jTextAreaChat);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(87, 87, 87)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(104, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addContainerGap(72, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jTextFieldMessageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldMessageActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldMessageActionPerformed
+
+    private void jButtonMessageSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonMessageSendActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonMessageSendActionPerformed
+
+    private void jButtonLoginLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLoginLogoutActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonLoginLogoutActionPerformed
+
+    private void newConnectionCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newConnectionCancelActionPerformed
+        jDialogNewConnection.setVisible(false);
+    }//GEN-LAST:event_newConnectionCancelActionPerformed
+
+    private void newConnectionConnectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newConnectionConnectActionPerformed
+        String userName = newConnectionUserName.getText().trim();
+        String serverAddress = newConnectionServerAddress.getText().trim();
+        String portNumber = newConnectionPort.getText().trim();
+        int port = 9090;
         
-        String output = msgOutputField.getText();
-        client.send(output);
-    }//GEN-LAST:event_jButton1ActionPerformed
-    
-    
+        if(userName.equalsIgnoreCase(""))
+        {
+        } else
+        {
+            if (serverAddress.equalsIgnoreCase(""))
+            {
+                serverAddress = "localhost";
+            }
+            try
+            {
+                port = Integer.parseInt(portNumber);
+            } catch (Exception en)
+            {
+                return;
+            }
+            
+            // Connect to server with the entered date for username, serveraddress and port
+            client = new EchoClient(serverAddress, port);
+            
+            // 
+            
+            jDialogNewConnection.setVisible(false);
+        }
+    }//GEN-LAST:event_newConnectionConnectActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -131,20 +303,29 @@ EchoClient client = new EchoClient();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButtonLoginLogout;
+    private javax.swing.JButton jButtonMessageSend;
+    private javax.swing.JDialog jDialogNewConnection;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea msgInputField;
-    private javax.swing.JTextArea msgOutputField;
+    private javax.swing.JTextArea jTextAreaChat;
+    private javax.swing.JTextField jTextFieldMessage;
+    private javax.swing.JButton newConnectionCancel;
+    private javax.swing.JButton newConnectionConnect;
+    private javax.swing.JTextField newConnectionPort;
+    private javax.swing.JTextField newConnectionServerAddress;
+    private javax.swing.JTextField newConnectionUserName;
     // End of variables declaration//GEN-END:variables
 
     @Override
-    public void messageArrived(String data) {
+    public void messageArrived(String data)
+    {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    public void setInput(String input)
-    {
-        msgInputField.setText(input);
-    }
+    
+    
 }
